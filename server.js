24745c751bf4130627fa3270a1a859994e0bba2c
eilon90 <eilon90@gmail.com>
2021-04-12 17:23:07 +0300

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require('./server/routes/api');
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://eilon90:${process.env.atlas}@mapstory.k44ic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })ף
+mongoose.connect(`mongodb+srv://eilon90:${process.env.atlas}@mapstory.k44ic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.static(path.join(__dirname, 'build')));
 
